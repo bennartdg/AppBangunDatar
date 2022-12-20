247f067a53be682f3430b34551387871fbdb58f7
Hilmy-19 <hilmyaiman625@gmail.com>
2022-12-20 09:01:23 +0700

    add feature lingkaran

---
+++ AppBangunDatar/src/main/java/com/oop/reguler/appbangundatar/Lingkaran.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.oop.reguler.appbangundatar;
+
+/**
+ *
+ * @author MEMi
+ */
+public class Lingkaran extends BangunDatar{
+    private double r;
+    public final double phi = 3.14;
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    @Override
+    double luas() {
+        return phi * r * r;
+    }
+
+    @Override
+    double keliling() {
+        return 2 * phi * r;
+    }
+    
+    
+}
