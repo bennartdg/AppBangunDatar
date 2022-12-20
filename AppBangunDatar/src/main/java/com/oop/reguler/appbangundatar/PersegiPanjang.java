7f9e5c5c5a31360b5b58ce80508678cd2233a10b
Bennart Gunawan <bennart.dgunawan@gmail.com>
2022-12-20 08:59:38 +0700

    add feature persegi & persegi panjang

---
+++ AppBangunDatar/src/main/java/com/oop/reguler/appbangundatar/PersegiPanjang.java
@@ -0,0 +1,37 @@
+package com.oop.reguler.appbangundatar;
+
+public class PersegiPanjang extends BangunDatar{
+  private double panjang;
+  private double lebar;
+
+  public PersegiPanjang() {
+  }
+
+  public double getPanjang() {
+    return panjang;
+  }
+
+  public void setPanjang(double panjang) {
+    this.panjang = panjang;
+  }
+
+  public double getLebar() {
+    return lebar;
+  }
+
+  public void setLebar(double lebar) {
+    this.lebar = lebar;
+  }
+  
+  
+  @Override
+  double luas() {
+    return panjang * lebar;
+  }
+
+  @Override
+  double keliling() {
+    return 2 * (panjang + lebar);
+  }
+  
+}
